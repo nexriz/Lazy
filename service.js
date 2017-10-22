d87b6481285f49d8c4b931ecd6f60e1a8fd47874
Mrlott <nexriz@hotmail.com>
2017-10-22 13:28:42 +0200

    m

--- service.js
+++ service.js
@@ -4,6 +4,6 @@ const path = require('path')
 const arg = process.argv[2]
 console.log(arg)
 if(arg)
-    exec('qckwinsvc --uninstall --name "lazy" --description "lazy server" --script \"' + path.resolve(__dirname, "Service.js") + "\"  --startImmediately", (err, data) => console.log(err, data))
+    exec('qckwinsvc --uninstall --name "lazy" --description "lazy server" --script \"' + path.resolve(__dirname, "server.js") + "\"  --startImmediately", (err, data) => console.log(err, data))
 else
-    exec('qckwinsvc --name "lazy" --description "lazy server" --script \"' + path.resolve(__dirname, "Service.js") + "\"  --startImmediately", (err, data) => console.log(err, data))
+    exec('qckwinsvc --name "lazy" --description "lazy server" --script \"' + path.resolve(__dirname, "server.js") + "\"  --startImmediately", (err, data) => console.log(err, data))
